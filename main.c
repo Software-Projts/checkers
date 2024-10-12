9c696f3f45f189a782a68c242fb0f26bba1badd8
Lee Nelson <awesomelee06@gmail>
2024-10-12 13:59:36 -0500

    ....

--- main.c
+++ main.c
@@ -1,49 +1,35 @@
 #include <stdio.h>
 
 char board[8][8];
-
+char makeBoard(char piece, char even, char odd, int outIt, int inIt);
 int main() {
     //Sets ups the board with pieces in place
     for(int i = 0; i < 8; i++) {
         for(int j = 0; j < 8; j++) {
+            //2, 4, 6, 8 rows
             if(i % 2 == 0) {
-                if(j % 2 == 0) {
-                    board[i][j] = '1';
-                    printf("%c  ", board[i][j]);
-                }else {
-                    if(j != 7) {
-                        if(j<=3) {
-                            board[i][j] = 'r';
-                            printf("%c  ", board[i][j]);
-                        }else {
-                            board[i][j] = '2';
-                            printf("%c  ", board[i][j]);
-                        }
-                    }else {
-                        board[i][j] = '2';
-                        printf("%c  ", board[i][j]);
-                    }
-                }
+                makeBoard('r', '1', '2', i, j);
+            // 1, 3, 5, 7 rows
              }else {
-                if(j % 2 == 0) {
-                    if(j>=3) {
-                         board[i][j] = 'r';
-                        printf("%c  ", board[i][j]);
-                    }else {
-                        board[i][j] = '2';
-                        printf("%c  ", board[i][j]);
-                    }
-                }else {
-                    if(j != 7) {
-                        board[i][j] = '1';
-                        printf("%c  ", board[i][j]);
-                    }else {
-                        board[i][j] = '1';
-                        printf("%c", board[i][j]);
-                    }
-                }
+                makeBoard('r', '2', '1', i, j);
              }
+                    
         }
         printf("\n");
+    }
+}
+
+char makeBoard(char piece, char even, char odd, int outIt, int inIt) {
+    if(inIt % 2 == 0) {
+        board[outIt][inIt] = even;
+        printf("%c  ", board[outIt][inIt]);
+    }else {
+        if(inIt != 7) {
+            board[outIt][inIt] = odd;
+            printf("%c  ", board[outIt][inIt]);
+        }else {
+                board[outIt][inIt] = odd;
+                printf("%c", board[outIt][inIt]);
+        }
     }
 }
