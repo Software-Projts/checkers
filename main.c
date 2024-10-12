ebffef018f418c6bd973db1ae15d28f07eb35d9b
bobsyashi <113643287+bobsyashi@users.noreply.github.com>
2024-10-12 10:27:11 -0500

    Update main.c

--- main.c
+++ main.c
@@ -6,10 +6,12 @@ int main() {
     for(int i = 0; i < 8; i++) {
         for(int j = 0; j < 8; j++) {
             board[i][j] = 1;
-            printf("%d", board[i][j]);
+            if(j != 7) {}
+                printf("%d     ", board[i][j]);
+            }else {
+                printf("%d", board[i][j]);
+            }
         }
         printf("\n");
     }
-    printf("Hi");
 }
-
