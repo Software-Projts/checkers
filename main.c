84eaae4584f91447b705a38519af1a807f9328ac
Lee Nelson <awesomelee06@gmail>
2024-10-12 13:27:20 -0500

    ..

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int board[8][8];
+char board[8][8];
 
 int main() {
     //Sets ups the board with pieces in place
@@ -8,28 +8,33 @@ int main() {
         for(int j = 0; j < 8; j++) {
             if(i % 2 == 0) {
                 if(j % 2 == 0) {
-                    board[i][j] = 1;
-                    printf("%d  ", board[i][j]);
+                    board[i][j] = '1';
+                    printf("%c  ", board[i][j]);
                 }else {
                     if(j != 7) {
-                        board[i][j] = 2;
-                        printf("%d  ", board[i][j]);
+                        if((j<=0 && j>=2)||(j>=5 && j<=7)) {
+                            board[i][j] = 'r';
+                            printf("%c  ", board[i][j]);
+                        }else {
+                            board[i][j] = '2';
+                            printf("%c  ", board[i][j]);
+                        }
                     }else {
-                        board[i][j] = 2;
-                        printf("%d", board[i][j]);
+                        board[i][j] = '2';
+                        printf("%c", board[i][j]);
                     }
                 }
              }else {
                 if(j % 2 == 0) {
-                    board[i][j] = 2;
-                    printf("%d  ", board[i][j]);
+                    board[i][j] = '2';
+                    printf("%c  ", board[i][j]);
                 }else {
                     if(j != 7) {
-                        board[i][j] = 1;
-                        printf("%d  ", board[i][j]);
+                        board[i][j] = '1';
+                        printf("%c  ", board[i][j]);
                     }else {
-                        board[i][j] = 2;
-                        printf("%d", board[i][j]);
+                        board[i][j] = '1';
+                        printf("%c", board[i][j]);
                     }
                 }
              }
