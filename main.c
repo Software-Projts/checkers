508300088c3447955e6c15f444134ede787cc200
Lee Nelson <awesomelee06@gmail>
2024-10-12 15:40:20 -0500

    Fix piece setup

--- main.c
+++ main.c
@@ -14,18 +14,18 @@ int main() {
             if(i % 2 == 0) {
                 
                 if((i == 6)) {
-                    makeBoard('r', '2', i, j);
+                    makeBoard('2', 'r', i, j);
                 }else if ((i==0)||(i==2)) {
-                    makeBoard('b', ' ', i, j);
+                    makeBoard(' ', 'b', i, j);
                 }else {
                     makeBoard('1', ' ', i, j);
                 }
             // 1, 3, 5, 7 rows
              }else {
                 if((i == 7)||(i == 5)) {
-                    makeBoard('2', 'r', i, j);
+                    makeBoard('r', '2', i, j);
                 }else if(i==1) {
-                    makeBoard('2', 'b', i, j);
+                    makeBoard('b', '2', i, j);
                 }else {
                     makeBoard('2', '1', i, j);
                 }
