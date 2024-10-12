b0ae40848b2eff915f1c17eda425341d9182d5e7
Lee Nelson <awesomelee06@gmail>
2024-10-12 18:38:12 -0500

    fohjiGS;ohewa

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 int piecNumber = 1;
-char board[8][8][4];
+char board[8][8];
+//add [] to board
 //Functions
 char makeBoard(char even, char odd, int outIt, int inIt);
 
