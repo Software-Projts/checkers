1ef0fe9d68f3917596c89d0c96476af96327798e
Lee Nelson <awesomelee06@gmail>
2024-10-13 22:31:26 -0500

    Same as last

--- main.c
+++ main.c
@@ -57,7 +57,7 @@ int main() {
 char makeBoard(char even, char odd, int outIt, int inIt) {
     if(inIt % 2 == 0) {
         if(inIt != 7) {
-            if(odd == 'r') {
+            if((odd == 'r')|| (odd == 'b')) {
                 pieceAndNum[0] = odd;
                 pieceAndNum[1] = '0' + pieceNum;
                 pieceAndNum[2] = '\0';
